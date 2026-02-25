6b5f3e6b23a9e42a7a718c1a0e388ed645adc56c
seammarahmad <se.ammarahmad@gmail.com>
2026-02-25 23:58:04 +0500

    DataBaseWorking Process

--- lib/Views/tweet/view/create_tweet_view.dart
+++ lib/Views/tweet/view/create_tweet_view.dart
@@ -4,6 +4,8 @@ import 'package:flutter_twitter_clone/Views/LoginViews/constants/constants.dart'
 import 'package:flutter_twitter_clone/Views/LoginViews/controller/auth_controller.dart';
 import 'package:flutter_twitter_clone/theme/pallete.dart';
 
+import '../../../model/usermodel.dart';
+
 class CreateTweetView extends ConsumerStatefulWidget {
   static String id = 'create_tweet_view';
 
@@ -16,34 +18,51 @@ class CreateTweetView extends ConsumerStatefulWidget {
 class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
   @override
   Widget build(BuildContext context) {
-    final currentUser=ref.watch(currentUserdetailsProvider).value;
+    final currentUser = ref.watch(currentUserdetailsProvider);
     return Scaffold(
-      backgroundColor: Pallete.backgroundColor,
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.close, color: Pallete.whiteColor),
-        ),
-        actions: [
-          RoundButton(colour: Pallete.blueColor, title: 'Tweet', onPress: () {  }, Size: 50.0,),
-        ],
-
-      ),
-      body: SafeArea(child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(currentUser.profilePic),
-                  radius: 30,
-                ),
-
-              ],
-            )
+        backgroundColor: Pallete.backgroundColor,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.close, color: Pallete.whiteColor),
+          ),
+          actions: [
+            RoundButton(colour: Pallete.blueColor,
+              title: 'Tweet',
+              onPress: () {},
+              Size: 50.0,),
           ],
+
         ),
-      )),
+        body: currentUser.when(
+        data: (currentUser)
+    {
+
+      final user = currentUser as UserModel;
+      return SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(user!.profilePic),
+                    radius: 30,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+    loading: () => const Center(
+    child: CircularProgressIndicator(),
+    ),
+    error: (error, stackTrace) => Center(
+    child: Text(error.toString()),
+    ),
+    ),
     );
   }
 }
