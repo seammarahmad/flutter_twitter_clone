afa6803f8f3d69e2d88cb2193b5ecb3735d2658a
seammarahmad <se.ammarahmad@gmail.com>
2026-03-05 00:02:30 +0500

    error resolved in create tweet page

--- lib/Views/tweet/view/create_tweet_view.dart
+++ lib/Views/tweet/view/create_tweet_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_twitter_clone/Views/LoginViews/constants/constants.dart';
 import 'package:flutter_twitter_clone/Views/LoginViews/controller/auth_controller.dart';
 import 'package:flutter_twitter_clone/theme/pallete.dart';
@@ -14,6 +15,14 @@ class CreateTweetView extends ConsumerStatefulWidget {
 }
 
 class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
+  final tweettextController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    tweettextController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserdetailsProvider);
@@ -28,14 +37,11 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
           icon: const Icon(Icons.close, color: Pallete.whiteColor),
         ),
         actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-            child: RoundButton(
-              colour: Pallete.blueColor,
-              title: 'Tweet',
-              onPress: () {},
-              Size: 50.0,
-            ),
+          RoundButton(
+            colour: Pallete.blueColor,
+            title: 'Tweet',
+            onPress: () {},
+            Size: 80.0,
           ),
         ],
       ),
@@ -46,7 +52,7 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
               child: Padding(
                 padding: EdgeInsets.all(20.0),
                 child: Text(
-                  "User profile data not found in database. Pleas```````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````                    `````````````````````````                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               e try logging out and signing up again.",
+                  "User profile data not found in database. Please try logging out and signing up again.",
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.white, fontSize: 16),
                 ),
@@ -64,22 +70,35 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: CircleAvatar(
+                          backgroundColor: Pallete.blueColor,
                           backgroundImage: user.profilePic.isNotEmpty
                               ? NetworkImage(user.profilePic)
                               : null,
-                          child: user.profilePic.isEmpty
-                              ? const Icon(Icons.person)
-                              : null,
                           radius: 30,
+                          child: user.profilePic.isEmpty
+                              ? const Icon(
+                                  Icons.person,
+                                  size: 30.0,
+                                  color: Colors.white,
+                                )
+                              : null,
                         ),
                       ),
-                      const Expanded(
+
+                      SizedBox(width: 20),
+                      Expanded(
                         child: TextField(
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                          decoration: InputDecoration(
+                          controller: tweettextController,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          decoration: const InputDecoration(
                             hintText: "What's happening?",
-                            hintStyle:
-                                TextStyle(color: Colors.grey, fontSize: 20),
+                            hintStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
                             border: InputBorder.none,
                           ),
                           maxLines: null,
@@ -107,9 +126,51 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
               TextButton(
                 onPressed: () => ref.refresh(currentUserdetailsProvider),
                 child: const Text("Retry"),
-              )
+              ),
             ],
           ),
+        ),
+      ),
+
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.only(bottom: 15.0),
+
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Pallete.greyColor,
+              width: 2.0,
+            ),
+          )
+        ),
+
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(
+                8.0,
+              ).copyWith(left: 15.0, right: 15.0),
+
+              child: SvgPicture.asset('assets/svgs/gallery.svg'),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(
+                8.0,
+              ).copyWith(left: 15.0, right: 15.0),
+
+              child: SvgPicture.asset('assets/svgs/gif.svg'),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(
+                8.0,
+              ).copyWith(left: 15.0, right: 15.0),
+
+              child: SvgPicture.asset('assets/svgs/emoji.svg'),
+            ),
+
+          ],
         ),
       ),
     );
